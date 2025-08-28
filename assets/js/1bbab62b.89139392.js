9558a7f43bcf380b7ff07ecad416485b1b6f701f
vidandco <vidandco@users.noreply.github.com>
2025-08-28 18:32:58 +0000

    deploy: 3a849082ea9253b28d9005fa45ee8c4e31825918

--- assets/js/1bbab62b.89139392.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[29],{6042:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/vida-website/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
