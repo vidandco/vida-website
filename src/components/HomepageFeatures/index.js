a276d21d1e64749ade8a50ad35d8b5ebb36a0770
vidandco <vidaandco@yahoo.ca>
2026-09-25 17:59:44 -0700

    Fix image URL case for Stonebridge feature
    Corrected image URL case for Stonebridge feature.

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Stonebridge & Mill Bay',
-    imageUrl: require('@site/static/img/Stonebridge.jpg').default,
+    imageUrl: require('@site/static/img/stonebridge.jpg').default,
     description: (
       <>
         For more information on Stonebridge Development and Mill Bay Homes, contact:{' '}
